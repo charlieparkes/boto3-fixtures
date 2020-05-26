a2f1b0bf466835ad6ad5d6f1a50cbc6725300279
Charlie <charlie@charliemathews.com>
2020-05-26 13:14:18 -0500

    Add pytest module and entry point

--- boto3_fixtures/contrib/pytest.py
+++ boto3_fixtures/contrib/pytest.py
@@ -1,1 +1,9 @@
-# TODO: pytest fixtures
+try:
+    import pytest
+except ImportError:
+    raise Exception("boto3_fixtures.contrib.pytest requires the pytest package")
+
+
+@pytest.fixture
+def boto3_fixture():
+    pass
